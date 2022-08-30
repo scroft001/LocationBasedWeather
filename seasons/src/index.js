29bed0b2f7012590327be9f5eda16e13042c97ca
Steven Croft <scroft001@gmail.com>
2022-08-29 22:20:37 -0700

    set state

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -6,16 +6,20 @@ class App extends React.Component{
     constructor(props){
         super(props);
 
+        // the only time we do direct assignment to this.state
         this.state = { lat: null };
+
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({lat: position.coords.latitude})
+            }, //Success callback
+            (err) => console.log(err)
+        );
     }
 
     //React says we  have to define render
     render(){
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position), //Success callback
-            (err) => console.log(err)
-        );
-        return <div>Latitude: </div>
+        return <div>Latitude: {this.state.lat}</div>
     }
 }
 
