d789647005875ba2c41b67675c23c33355429cb7
Steven Croft <scroft001@gmail.com>
2022-08-29 21:55:13 -0700

    Functional to class based component

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM  from "react-dom";
 
-const App = () =>{
-    // call to geo api
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position), //Success callback
-        (err) => console.log(err)
-    );
 
-    return <div>Hi There</div>
+class App extends React.Component{
+    render(){
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position), //Success callback
+            (err) => console.log(err)
+        );
+        return <div>Latitude: </div>
+    }
 }
 
 ReactDOM.render(
