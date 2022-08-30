8b7c3cb8d1722d8e862cb78fc20b565950681cc9
Steven Croft <scroft001@gmail.com>
2022-08-29 21:44:49 -0700

    Geolocation api

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,6 +2,12 @@ import React from "react";
 import ReactDOM  from "react-dom";
 
 const App = () =>{
+    // call to geo api
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => console.log(position), //Success callback
+        (err) => console.log(err)
+    );
+
     return <div>Hi There</div>
 }
 
