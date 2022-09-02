2bd6f1871bec0dc109b45605aee882a04a77a70c
Steven Croft <scroft001@gmail.com>
2022-09-01 22:10:34 -0700

    Remove conditionals at render

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM  from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
 import Spinner from "./Spinner";
+import './style/App.css'
 
 
 class App extends React.Component{
@@ -14,8 +15,7 @@ class App extends React.Component{
         );
     }
 
-    //React says we  have to define render
-    render(){
+    renderContent(){
         if(this.state.errorMessage && !this.state.lat){
             return <div>Error: {this.state.errorMessage}</div>;
         }
@@ -25,6 +25,15 @@ class App extends React.Component{
         return(
             <Spinner message="Please accept location request"/>
         );
+    }
+
+    //React says we  have to define render
+    render(){
+        return(
+            <div className="">
+                {this.renderContent()}
+            </div>
+        )
     }
 }
 
